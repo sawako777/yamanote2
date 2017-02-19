4d5805cc149f0436eaf04c60c0eaffc91e94a594
鴻池佐和子 <sawako@kounoikesawako-no-MacBook-Pro.local>
2017-02-19 16:43:18 +0900

    add user info colomn to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218132544) do
+ActiveRecord::Schema.define(version: 20170219074055) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 20170218132544) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "family_name",            limit: 255
+    t.string   "first_name",             limit: 255
+    t.string   "family_name_kana",       limit: 255
+    t.string   "first_name_kana",        limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
