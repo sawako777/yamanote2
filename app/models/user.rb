637160de988700d7869e7ef20909bba9f7e51890
鴻池佐和子 <sawako@kounoikesawako-no-MacBook-Pro.local>
2017-02-19 16:53:36 +0900

    make def name and fix user info on show.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+
+  def name
+    "#{family_name} #{first_name}"
+  end
+
+  def name_kana
+    "#{family_name_kana} #{first_name_kana}"
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
