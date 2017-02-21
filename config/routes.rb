0b716663f9bd4e12fb1d2707e70aeaa30517136c
鴻池佐和子 <sawako@kounoikesawako-no-MacBook-Pro.local>
2017-02-21 20:51:43 +0900

    make posts table and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   root to: "top#index"
+  get  'posts/new'    => 'tweets#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219085339) do
+ActiveRecord::Schema.define(version: 20170221114655) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "text",       limit: 65535
+    t.text     "image",      limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id",    limit: 4
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
