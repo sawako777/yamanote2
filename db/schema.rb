62bc28e9926836520eb4f3a47c3a7cca8186ce99
鴻池佐和子 <sawako@kounoikesawako-no-MacBook-Pro.local>
2017-02-21 21:55:47 +0900

    add gem paperclip, fix posts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221114655) do
+ActiveRecord::Schema.define(version: 20170221125213) do
 
   create_table "posts", force: :cascade do |t|
-    t.text     "text",       limit: 65535
-    t.text     "image",      limit: 65535
+    t.text     "text",               limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id",    limit: 4
+    t.integer  "user_id",            limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
