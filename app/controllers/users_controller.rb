8bb72a2559270e9158c22d008f630fbd0def342d
鴻池佐和子 <sawako@kounoikesawako-no-MacBook-Pro.local>
2017-02-28 08:10:47 +0900

    fix user page to show his posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def edit
