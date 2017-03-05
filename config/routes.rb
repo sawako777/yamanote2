b765402083b3310016eb64b71788aee0d253066f
鴻池佐和子 <sawako@kounoikesawako-no-MacBook-Pro.local>
2017-03-05 19:50:40 +0900

    change routes get yamanames to resources :yamanames

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'posts'        => 'posts#index'
   get 'posts/new'    => 'posts#new'
   post 'posts'       => 'posts#create'
-  get 'yamanames'     => 'yamanames#index'
+  resources :yamanames
 end
